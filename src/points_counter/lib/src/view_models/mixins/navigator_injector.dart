05f5e0b41079b743dcec054b44016e9f19aec72a
cirate <mateuszlampart@outlook.com>
2019-07-07 10:39:49 +0200

    Null check has been added to NavigatorInjector.

--- src/points_counter/lib/src/view_models/mixins/navigator_injector.dart
+++ src/points_counter/lib/src/view_models/mixins/navigator_injector.dart
@@ -2,9 +2,9 @@ import 'package:flutter/widgets.dart';
 
 mixin NavigatorInjector {
   NavigatorState _navigator;
-  NavigatorState get navigator => _navigator;
+  NavigatorState get navigator => _navigator ?? (throw ArgumentError.notNull('navigator'));
 
   void injectNavigator(NavigatorState navigator) {
-    this._navigator = navigator;
+    this._navigator = navigator ?? (throw ArgumentError.notNull('navigator'));
   }
 }
